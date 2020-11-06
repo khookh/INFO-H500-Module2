dc62e2082926e9b6ae9c90b43c6d2d9077856e74
sdonne <stefanodonne@gmail.com>
2020-11-06 15:29:55 +0100

    Adds filters

--- lab.py
+++ lab.py
@@ -1,23 +1,38 @@
+import os.path
 from tkinter import *
-from skimage.io import imread, imshow, imsave
+from tkinter.ttk import Separator
 import cv2 as cv
 import numpy as np
-import matplotlib.pyplot as plt
 from skimage.transform import rescale
 from scipy.signal import convolve2d
 
 im = cv.imread('portrait-Donald-Trump.jpg')
 
 
+def save():
+    count = 0
+    string_name = 'modified.jpg'
+    while os.path.isfile(string_name) is True:
+        count += 1
+        string_name = 'modified%d.jpg' % count
+    cv.imwrite('modified%d.jpg' % count, im)
+    print('modified%d.jpg' % count)
+
+
 def resize_im():
     global im
-    im = ((rescale(im, w.get(),multichannel=True))*255).astype('uint8')
+    if w.get() != 0:
+        im = ((rescale(im, w.get(), multichannel=True)) * 255).astype('uint8')
 
 
 def refresh():
     global im
-    print("refresh")
-    im = cv.imread('portrait-Donald-Trump.jpg')
+    temp = cv.imread('portrait-Donald-Trump.jpg')
+    if w.get() == 0:
+        size = 1
+    else:
+        size = w.get()
+    im = ((rescale(temp, size, multichannel=True)) * 255).astype('uint8')
 
 
 # cumulative histogram
@@ -47,9 +62,6 @@ def neg_img():
 def auto_level():
     global im
     ch = cumul_hist()
-    plt.figure()
-    plt.plot(ch)
-    plt.savefig("cumulplot.png")
 
     for v in range(256):
         if ch[v] > 0.05: break
@@ -76,18 +88,26 @@ def equalization_hist():
 
 
 # kernel mean filter
-def mean_filter(ksize):
+def mean_filter():
+    try:
+        ksize = int(ksize_in.get())
+    except:
+        ksize = 3
     global im
     kernel = np.ones((ksize, ksize))
     kernel /= kernel.sum()
     meaned = np.zeros(im.shape)
     for dim in range(im.shape[2]):
         meaned[:, :, dim] = convolve2d(im[:, :, dim], kernel, "same")
-    im = meaned
+    im = meaned.astype('uint8')
 
 
 # kernel median filter
-def median_filter(ksize):
+def median_filter():
+    try:
+        ksize = int(ksize_in.get())
+    except:
+        ksize = 3
     global im
     output = np.zeros(im.shape)
     print("processing.", end="")
@@ -97,22 +117,36 @@ def median_filter(ksize):
             for dim in range(im.shape[2]):
                 output[i, j, dim] = np.median(im[i - ksize // 2:i + ksize // 2, j - ksize // 2:j + ksize // 2, dim])
     print("median filter applied succesfully")
-    im = output
+    im = output.astype('uint8')
 
 
 # GUI
 wd = Tk()
+ksize_label = Label(wd, text="Kernel Size")
+ksize_label.pack()
+ksize_in = Entry(wd)
+ksize_in.pack()
+b_mean = Button(wd, text="mean filter", command=mean_filter)
+b_mean.pack()
+b_median = Button(wd, text="median filter", command=median_filter)
+b_median.pack()
+sep = Separator(wd)
+sep.pack()
 B = Button(wd, text="auto-level", command=auto_level)
-C = Button(wd, text="equalization", command=equalization_hist)
-D = Button(wd, text="refresh", command=refresh)
-w = Scale(wd, from_=0, to=2, orient=HORIZONTAL,resolution=0.1)
-G = Button(wd, text="Apply rescale", command=resize_im)
-w.pack()
-G.pack()
-D.pack()
-C.pack()
 B.pack()
+C = Button(wd, text="equalization", command=equalization_hist)
+C.pack()
+D = Button(wd, text="refresh", command=refresh)
+D.pack()
+
+w = Scale(wd, from_=0, to=2, orient=HORIZONTAL, resolution=0.1)
+w.pack()
+G = Button(wd, text="Apply rescale", command=resize_im)
+G.pack()
+S = Button(wd, text="Save", command=save)
+S.pack()
 cv.namedWindow('Image')
+
 while 1:
     wd.update_idletasks()
     wd.update()
